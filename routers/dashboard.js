f44eff02ec28cad13abcf060edc2215ab12cd67f
D.hacThe <hienthe473@gmail.com>
2022-06-14 20:13:08 +0700

    edit dashboard link

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -1,7 +1,7 @@
 const { AuthMiddleware } = require("../helper/JWT");
 const express = require("express");
 const router = express.Router();
-const DashBoardController = require("../controllers/DashBoardController");
+const DashBoardController = require("../controllers/DashboardController");
 
 router.get("/general", AuthMiddleware, DashBoardController.getGeneralReport);
 router.get("/timely", AuthMiddleware, DashBoardController.getTimelyReport);
